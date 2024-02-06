dfcb966a09ec6f2d75c3ff9e0ec16c4ec88f5d21
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-02-05 20:58:33 -0700

    Update GARCH Strategy

--- 2_strategies/strat_garch.py
+++ 2_strategies/strat_garch.py
@@ -260,13 +260,32 @@ for i in data.index.get_level_values('date').unique().tolist():
 filtered_df = data[data['cluster'] == 3].copy()
 filtered_df = filtered_df.reset_index(level=1)
 filtered_df.index = filtered_df.index+pd.DateOffset(1)
-filtered_df.reset_index().set_index(['date', 'ticker'])
+filtered_df = filtered_df.reset_index().set_index(['date', 'ticker'])
 dates = filtered_df.index.get_level_values('date').unique().tolist()
 fixed_dates = {}
 for d in dates:
     fixed_dates[d.strftime('%Y-%m-%d')] = filtered_df.xs(d, level=0).index.tolist()
-print(filtered_df)
+# print(filtered_df)
+# print(fixed_dates)
 
+# Define Portfolio Optimization function
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
+
+def optimize_weights(prices, lower_bound=0):
+    returns = expected_returns.mean_historical_return(prices=prices,
+                                                      frequency=252)
+    cov = risk_models.sample_cov(prices=prices,
+                                 frequency=252)
+    
+    ef = EfficientFrontier(expected_returns=returns,
+                           cov_matrix=cov,
+                           weight_bounds=(lower_bound, .1), # Makes sure our portfolio is diversified 10% being the highest bound and lower bound of 2.5%
+                           solver='SCS')
+    
+    weights = ef.max_sharpe()
+    returns ef.clean_weights()
 
 
 
