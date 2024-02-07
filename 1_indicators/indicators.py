b752585bcd74f53c6cc73e1b8e293605cde0da95
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-02-07 16:12:53 -0700

    Update Indicators

--- 1_indicators/indicators.py
+++ 1_indicators/indicators.py
@@ -3,42 +3,53 @@ import pandas as pd
 import numpy as np
 import pandas_ta
 
-
 # Garman-Klass Volatility Indicator - particularly useful for assets with significant overnight price movements or markets that are open 24/7
 def garman_klass_volatility(data):
-    vol = ((np.log(data['high']) - np.log(data['low'])) ** 2) / 2 - (2 * np.log(2) - 1) * ((np.log(data['adj close']) - np.log(data['open'])) ** 2)
-    return vol
-df['garman_klass_vol'] = ((np.log(df['high'])-np.log(df['low']))**2)/2-(2*np.log(2)-1)*((np.log(df['adj close'])-np.log(df['open']))**2)
+    volatility = ((np.log(data['high']) - np.log(data['low'])) ** 2) / 2 - (2 * np.log(2) - 1) * ((np.log(data['adj close']) - np.log(data['open'])) ** 2)
+    return volatility
 # print(data)
 
 # RSI - identify potential buying or selling opportunities, often used to determine overbought and oversold conditions
-df['rsi'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.rsi(close=x, length=20))
-# print(df)
-# df.xs('AAPL', level=1)['rsi'].plot()
+def relative_strength_index(data, length=20):
+    rsi = data.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.rsi(close=x, length=length))
+    return rsi
+# print(data)
+# data.xs('AAPL', level=1)['rsi'].plot()
 # plt.show()
 
 # Bollinger Bands - Primarily used to spot reversals, corrections, and potential entry/exit points based on momentum.
-df['bb_low'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 0])
-df['bb_mid'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 1])
-df['bb_high'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:, 2])
-# print(df)
+def bollinger_bands(data, length=20):
+    bbands = data.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=length))
+    bb_low = bbands.iloc[:, 0]
+    bb_mid = bbands.iloc[:, 1]
+    bb_high = bbands.iloc[:, 2]
+    return bb_low, bb_mid, bb_high
+# print(data)
 
 # ATR- A rule of thumb is to multiply the ATR by two to determine a reasonable stop-loss point. So if you're buying a stock, you might place a stop-loss at a level twice the ATR below the entry price. If you're shorting a stock, you would place a stop-loss at a level twice the ATR *******8
-def compute_atr(stock_data):
-    atr = pandas_ta.atr(high=stock_data['high'],
-                      low=stock_data['low'],
-                      close=stock_data['close'],
-                      length=14)
-    return atr.sub(atr.mean()).div(atr.std())
-df['atr'] = df.groupby(level=1, group_keys=False).apply(compute_atr)
+def average_true_range(data, length=14):
+    atr = data.groupby(level=1, group_keys=False).apply(lambda x: pandas_ta.atr(high=x['high'], low=x['low'], close=x['close'], length=length))
+    return atr
 
 # MACD - Uses two moving avgs to identify momentum and reversal points
-def compute_macd(close):
-    macd = pandas_ta.macd(close=close, length=20).iloc[:,0]
-    return macd.sub(macd.mean()).div(macd.std())
-df['macd'] = df.groupby(level=1, group_keys=False)['adj close'].apply(compute_macd)
+def moving_average_convergence_divergence(data, length=20):
+    macd = data.groupby(level=1, group_keys=False)['adj close'].apply(lambda x: pandas_ta.macd(close=x, fast=length).iloc[:, 0])
+    return macd
 
 # Dollar Volume - Price of stock * Volume to obtain its Market Cap
-df['dollar_volume'] = (df['adj close']*df['volume'])/1e6
-# print(df)
-# print(df.sort_values(by='dollar_volume', descending=True))
+def dollar_volume(data):
+    dv = (data['adj close'] * data['volume']) / 1e6
+    return dv
+# print(data)
+# print(data.sort_values(by='dollar_volume', descending=True))
+
+
+'''
+====================== EXAMPLE OF USE ======================
+df['garman_klass_vol'] = garman_klass_volatility(df)
+df['rsi'] = relative_strength_index(df)
+df[['bb_low', 'bb_mid', 'bb_high']] = bollinger_bands(df)
+df['atr'] = average_true_range(df)
+df['macd'] = moving_average_convergence_divergence(df)
+df['dollar_volume'] = dollar_volume(df)
+'''
