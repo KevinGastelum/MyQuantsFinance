e9219a98b9960407f8e71a234d1f83b3328277c3
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-02-12 21:35:07 -0700

    Example Autogen Script

--- 4_llm/autogen_ai.py
+++ 4_llm/autogen_ai.py
@@ -7,4 +7,4 @@ config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
 user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding", "use_docker": False}) # IMPORTANT: set to True to run code in docker, recommended
 user_proxy.initiate_chat(assistant, message="Plot a chart of NVDA and TESLA stock price change YTD.")
-# This initiates an automated chat between the two agents to solve the task
+# This initiates an automated chat between the two agents to solve the task 
