863789b103d3f477310349f64c8d0b6b3878a13c
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-02-15 23:36:21 -0700

    FinBert ML Sentiment Strat

--- 2_strategies/strat_sentiment_lumibot.py
+++ 2_strategies/strat_sentiment_lumibot.py
@@ -55,7 +55,7 @@ def estimate_sentiment(news):
 
 
 if __name__ == "__main__":
-    tensor, sentiment = estimate_sentiment(['markets responded negatively to the news!','traders were pleasently surprised!'])
+    tensor, sentiment = estimate_sentiment(['markets responded positively to the news!','traders were pleasently surprised!'])
     print(tensor, sentiment)
     print(torch.cuda.is_available())
 
