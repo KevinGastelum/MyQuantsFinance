3d4fb1b4c96a1464b6f2da255b310d9a01247a60
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-02-15 22:32:24 -0700

    FinBert ML Sentiment Strat

--- 2_strategies/test.py
+++ 2_strategies/test.py
@@ -6,24 +6,20 @@ import torch
 # device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # print(device)
 
-print("Torch version:",torch.__version__)
+print("Torch version:",torch.__version__) 
+# Expected Output = torch version+cuda
 
-print("Is CUDA enabled?",torch.cuda.is_available())
+print("Is cuda enabled?", torch.cuda.is_available()) 
+# Expected Output = True
 
+print(torch.cuda.device_count()) 
+# Expected Output = 1
 
+print(torch.cuda.current_device()) 
+# Expected Output = 0
 
+print(torch.cuda.device(0)) 
+# Expected Output = instance_name
 
-# print(torch.cuda.is_available()) 
-# # Expected Output = True
-
-# print(torch.cuda.device_count()) 
-# # Expected Output = 1
-
-# print(torch.cuda.current_device()) 
-# # Expected Output = 0
-
-# print(torch.cuda.device(0)) 
-# # Expected Output = instance_name
-
-# print(torch.cuda.get_device_name()) 
-# # Expected = 2070 rtx
+print(torch.cuda.get_device_name()) 
+# Expected = 2070 rtx
