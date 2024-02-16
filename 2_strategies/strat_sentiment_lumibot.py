82ce770133b839bab32f1bbb57e3d95f799af629
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-02-15 23:10:28 -0700

    FinBert ML Sentiment Strat

--- 2_strategies/strat_sentiment_lumibot.py
+++ 2_strategies/strat_sentiment_lumibot.py
@@ -8,9 +8,11 @@ from lumibot.traders import Trader
 from lumibot.backtesting import YahooDataBacktesting
 from alpaca_trade_api import REST
 from timedelta import Timedelta
-# from finbert_utils import estimate_sentiment
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from typing import Tuple 
 
-# Load env
+# Load APIs
 load_dotenv()
 API_KEY = os.getenv('ALPACA_KEY')
 API_SECRET = os.getenv('ALPACA_SECRET')
@@ -24,104 +26,69 @@ ALPACA_CREDS = {
 }
 
 
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
-from typing import Tuple 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# Strategy
+class MLTrader(Strategy):
+  def initialize(self, symbol:str="SPY", cash_at_risk:float=.5):
+    self.symbol = symbol
+    self.sleeptime = "24H" # Adjust SleepTime
+    self.last_trade = None
+    self.cash_at_risk = cash_at_risk # Currently set at 1%
+    self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
 
-tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
-model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert").to(device)
-labels = ["positive", "negative", "neutral"]
-
-def estimate_sentiment(news):
-    if news:
-        tokens = tokenizer(news, return_tensors="pt", padding=True).to(device)
-
-        result = model(tokens["input_ids"], attention_mask=tokens["attention_mask"])[
-            "logits"
-        ]
-        result = torch.nn.functional.softmax(torch.sum(result, 0), dim=-1)
-        probability = result[torch.argmax(result)]
-        sentiment = labels[torch.argmax(result)]
-        return probability, sentiment
-    else:
-        return 0, labels[-1]
+# Returns Cash bal, Price of asset, Qty to buy == %1 of my bal
+  def position_sizing(self):
+    cash = self.get_cash() # My Balance
+    last_price = self.get_last_price(self.symbol)
+    quantity = floor(cash * self.cash_at_risk / last_price) # ADD leverage * calc
+    return cash, last_price, quantity
 
 
-if __name__ == "__main__":
-    tensor, sentiment = estimate_sentiment(['markets responded negatively to the news!','traders were displeased!'])
-    print(tensor, sentiment)
-    print(torch.cuda.is_available())
+  # Set Dates / Get News / Calc Sentiment
+  def get_dates(self):
+    today = self.get_datetime()
+    x_days_prior = today - Timedelta(days=3) # News outlook days
+    return today.strftime('%Y-%m-%d'), x_days_prior.strftime('%Y-%m-%d')
 
-class MLTrader(Strategy): 
-    def initialize(self, symbol:str="SPY", cash_at_risk:float=.5): 
-        self.symbol = symbol
-        self.sleeptime = "24H" 
-        self.last_trade = None 
-        self.cash_at_risk = cash_at_risk
-        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
+  def get_news(self):
+    today, x_days_prior = self.get_dates()
+    news = self.api.get_news(symbol=self.symbol, start=x_days_prior, end=today)
+    news = [ev.__dict__["_raw"]["headline"] for ev in news]
+    return news
 
-    def position_sizing(self): 
-        cash = self.get_cash() 
-        last_price = self.get_last_price(self.symbol)
-        quantity = round(cash * self.cash_at_risk / last_price,0)
-        return cash, last_price, quantity
 
-    def get_dates(self): 
-        today = self.get_datetime()
-        three_days_prior = today - Timedelta(days=3)
-        return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
+# Initialize Trade order
+  def on_trading_iteration(self):
+    cash, last_price, quantity = self.position_sizing()
 
-    def get_sentiment(self): 
-        today, three_days_prior = self.get_dates()
-        news = self.api.get_news(symbol=self.symbol, 
-                                 start=three_days_prior, 
-                                 end=today) 
-        news = [ev.__dict__["_raw"]["headline"] for ev in news]
-        probability, sentiment = estimate_sentiment(news)
-        return probability, sentiment 
+    if cash > last_price:
+        if self.last_trade == None:
+          news = self.get_news()
+          print(news)
+          order = self.create_order(
+              self.symbol,
+              quantity,
+              "buy",
+              order_type="bracket",
+              take_profit_price=last_price*1.20, # TakeProfit 4 BUY = %20
+              stop_loss_price=last_price*.95 # StopLoss = %5
+          )
+          self.submit_order(order)
+          self.last_trade = "buy"
 
-    def on_trading_iteration(self):
-        cash, last_price, quantity = self.position_sizing() 
-        probability, sentiment = self.get_sentiment()
 
-        if cash > last_price: 
-            if sentiment == "positive" and probability > .999: 
-                if self.last_trade == "sell": 
-                    self.sell_all() 
-                order = self.create_order(
-                    self.symbol, 
-                    quantity, 
-                    "buy", 
-                    type="bracket", 
-                    take_profit_price=last_price*1.20, 
-                    stop_loss_price=last_price*.95
-                )
-                self.submit_order(order) 
-                self.last_trade = "buy"
-            elif sentiment == "negative" and probability > .999: 
-                if self.last_trade == "buy": 
-                    self.sell_all() 
-                order = self.create_order(
-                    self.symbol, 
-                    quantity, 
-                    "sell", 
-                    type="bracket", 
-                    take_profit_price=last_price*.8, 
-                    stop_loss_price=last_price*1.05
-                )
-                self.submit_order(order) 
-                self.last_trade = "sell"
-
-start_date = datetime(2020,1,1)
-end_date = datetime(2023,12,31) 
-broker = Alpaca(ALPACA_CREDS) 
-strategy = MLTrader(name='mlstrat', broker=broker, 
-                    parameters={"symbol":"SPY", 
+# Date range
+start_date = datetime(2023, 12, 15)
+end_date = datetime(2023, 12, 31)
+# Broker
+broker = Alpaca(ALPACA_CREDS)
+strategy = MLTrader(name='mlstrat', broker=broker,
+                    parameters={"symbol":"SPY",
                                 "cash_at_risk":.5})
+
+# Backtest
 strategy.backtest(
-    YahooDataBacktesting, 
-    start_date, 
-    end_date, 
+    YahooDataBacktesting,
+    start_date,
+    end_date,
     parameters={"symbol":"SPY", "cash_at_risk":.5}
-)
+    )
