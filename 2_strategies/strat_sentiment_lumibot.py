29197703e11e0d9d7a0c3b770b36ce070cf36af9
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2024-02-16 18:19:33 -0700

    FinBert ML Sentiment Strat

--- 2_strategies/strat_sentiment_lumibot.py
+++ 2_strategies/strat_sentiment_lumibot.py
@@ -6,7 +6,7 @@ import pandas
 from lumibot.strategies.strategy import Strategy
 from lumibot.brokers import Alpaca
 from lumibot.traders import Trader
-from lumibot.assets import Asset
+from lumibot.entities import Asset
 from lumibot.backtesting import YahooDataBacktesting
 from alpaca_trade_api import REST
 from timedelta import Timedelta
@@ -54,19 +54,12 @@ def estimate_sentiment(news):
     else:
         return 0, labels[-1]
 
-# Test Sentiment
-# if __name__ == "__main__":
-#     tensor, sentiment = estimate_sentiment(['markets responded positively to the news!','traders were pleasantly surprised!'])
-#     print(tensor, sentiment)
-#     print(torch.cuda.is_available())
-
-
 
 # Strategy
 class MLTrader(Strategy):
-  def initialize(self, symbol:str="BTC", cash_at_risk:float=.2):
+  def initialize(self, symbol:str="BTC", cash_at_risk:float=.5):
     self.symbol = symbol
-    self.sleeptime = "8H" # Adjust SleepTime
+    self.sleeptime = "24H" # Adjust SleepTime
     self.last_trade = None
     self.cash_at_risk = cash_at_risk # Currently set at 1%
     self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
@@ -97,49 +90,52 @@ class MLTrader(Strategy):
   def on_trading_iteration(self):
     cash, last_price, quantity = self.position_sizing()
     probability, sentiment = self.get_sentiment()
+    crypto_asset = Asset(symbol=self.symbol, asset_type='crypto')
+
     # Make sure we have enough funds to exec trade
     if cash > last_price:
         if sentiment == "positive" and probability > .999:
             if self.last_trade == "sell":
                   self.sell_all()
-            order = self.create_order(
-                self.symbol,
+            order = self.create_order( # SELL if Sentiment Negative
+                # self.symbol,
+                crypto_asset,
                 quantity,
                 "buy",
                 type="bracket",
-                take_profit_price=last_price*1.20, # TakeProfit 4 BUY = %20
-                stop_loss_price=last_price*.95 # StopLoss = %5 increase
+                take_profit_price=last_price * 1.20, # TakeProfit 4 BUY = %20
+                stop_loss_price=last_price * .95 # StopLoss = %5 increase
             )
             self.submit_order(order)
             self.last_trade = "buy"
-        # Negative Sentiment == BUY
-        elif sentiment == "negative" and probability > .999:
+        elif sentiment == "negative" and probability > .999: # BUY if Sentiment Positive
             if self.last_trade == "buy":
                   self.sell_all()
             order = self.create_order(
-                self.symbol,
+                # self.symbol,
+                crypto_asset,
                 quantity,
                 "sell",
                 type="bracket",
-                take_profit_price=last_price*.8, # TakeProfit 4 SELL = %20
-                stop_loss_price=last_price*1.05 # StopLoss = %5 drop
+                take_profit_price=last_price * .8, # TakeProfit 4 SELL = %20
+                stop_loss_price=last_price * 1.05 # StopLoss = %5 drop
             )
             self.submit_order(order)
             self.last_trade = "sell"
 
 
 # Date range && Broker
-start_date = datetime(2022, 1, 1)
+start_date = datetime(2020, 1, 1)
 end_date = datetime(2023, 12, 31)
 broker = Alpaca(ALPACA_CREDS)
 strategy = MLTrader(name='mlstrat', broker=broker,
                     parameters={"symbol":"BTC",
-                                "cash_at_risk":.2})
+                                "cash_at_risk":.5})
 
 # Backtest
 strategy.backtest(
     YahooDataBacktesting,
     start_date,
     end_date,
-    parameters={"symbol":"BTC", "cash_at_risk":.2}
+    parameters={"symbol":"BTC-USDT", "cash_at_risk":.5}
     )
