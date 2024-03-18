ebd8aeae5c1847ac293a42660ea5bb6c3a27731a
KevinGastelum <kev.gas777@gmail.com>
2024-03-18 10:26:23 -0700

    Strategy Tests

---
+++ 6_notebooks/strat.py
@@ -0,0 +1,58 @@
+# import libs
+import os
+import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+# QuantFreedom
+from quantfreedom.enums import CandleBodyType
+from quantfreedom.helper_funcs import dl_ex_candles, cart_products
+from quantfreedom.indicators.tv_indicators import macd_tv, ema_tv
+from quantfreedom.strategies.strategy import Strategy
+
+from logging import getLogger
+from typing import NamedTuple
+
+# from dotenv import load_dotenv
+# load_dotenv()
+# api_key = os.getenv('MFX_KEY')
+# api_secret = os.getenv('MFX_SECRET')
+
+logger = getLogger('info')
+
+class IndicatorSettingsArray(NamedTuple):
+    ema_length: np.array
+    fast_length: np.array
+    macd_below: np.array
+    signal_smoothing: np.array
+    slow_length: np.array
+
+class MACDandEMA(Strategy):
+    ema_length = None
+    fast_length = None
+    macd_below = None
+    signal_smoothing = None
+    slow_length = None
+
+    def __init__(
+        self,
+        long_short: str,
+        ema_length: np.array,
+        fast_length: np.array,
+        macd_below: np.array,
+        signal_smoothing: np.array,
+        slow_length: np.array,
+      ):
+        self.long_short = long_short
+        self.log_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+        cart_arrays = cart_product(
+            ema_length=ema_length,
+            fast_length=fast_length,
+            macd_below=macd_below,
+            signal_smoothing=signal_smoothing,
+            slow_length=slow_length,
+        )
+
+        cart_arrays = cart_arrays.T[cart_arrays[1] < cart_arrays[4]].T
+
+
