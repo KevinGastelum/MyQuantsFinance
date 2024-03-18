35b23919cda87d5b7c9d889e83c296afe813c778
KevinGastelum <kev.gas777@gmail.com>
2024-03-18 10:35:31 -0700

    Test Strategy QuantFreedom

--- 6_notebooks/strat.py
+++ 6_notebooks/strat.py
@@ -16,10 +16,10 @@ from typing import NamedTuple
 # load_dotenv()
 # api_key = os.getenv('MFX_KEY')
 # api_secret = os.getenv('MFX_SECRET')
-
+# Min 31
 logger = getLogger('info')
 
-class IndicatorSettingsArray(NamedTuple):
+class IndicatorSettingsArrays(NamedTuple):
     ema_length: np.array
     fast_length: np.array
     macd_below: np.array
@@ -46,13 +46,25 @@ class MACDandEMA(Strategy):
         self.log_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
         cart_arrays = cart_product(
-            ema_length=ema_length,
-            fast_length=fast_length,
-            macd_below=macd_below,
-            signal_smoothing=signal_smoothing,
-            slow_length=slow_length,
+            named_tuple=IndicatorSettingsArrays(
+                ema_length=ema_length,
+                fast_length=fast_length,
+                macd_below=macd_below,
+                signal_smoothing=signal_smoothing,
+                slow_length=slow_length,
+            )
         )
 
         cart_arrays = cart_arrays.T[cart_arrays[1] < cart_arrays[4]].T
+
+        self.indicator_settings_arrays: IndicatorSettingsArrays = IndicatorSettingsArrays(
+            ema_length=cart_arrays[0].astype(np.int_),
+            fast_length=cart_arrays[1].astype(np.int_),
+            macd_below=cart_arrays[2].astype(np.int_),
+            signal_smoothing=cart_arrays[3].astype(np.int_),
+            slow_length=cart_arrays[4].astype(np.int_),
+        )
+
+        
 
 
